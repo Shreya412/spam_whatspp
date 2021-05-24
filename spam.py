b1f924321ebc9c1dfb77b24016862934d9cc2a07
Shreya Parmar <72817935+Shreya412@users.noreply.github.com>
2021-05-24 13:02:41 +0530

    Add files via upload

---
+++ spam.py
@@ -0,0 +1,18 @@
+import pyautogui, time
+import webbrowser as wb
+wb.open("web.whatsapp.com")
+time.sleep(25)
+for i in range(50):
+    pyautogui.press("i")
+    pyautogui.press("enter")
+    pyautogui.press("l")
+    pyautogui.press("o")
+    pyautogui.press("v")
+    pyautogui.press("e")
+    pyautogui.press("enter")
+    pyautogui.press("u")
+    pyautogui.press("enter")
+    pyautogui.press("b")
+    pyautogui.press("r")
+    pyautogui.press("o")
+    pyautogui.press("enter")
